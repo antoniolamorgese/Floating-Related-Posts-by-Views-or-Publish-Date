348d71ec049deb2e393dc5955f55389d32c5d9b4
antoniolamorgese <34546111+antoniolamorgese@users.noreply.github.com>
2023-01-26 10:41:02 +0100

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -9,4 +9,15 @@ if( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
 	exit();
 
 // Delete option plugin
-delete_option( 'floating_related_posts_by_views_or_date_option_name' );
+if (is_multisite()) {
+	$blogs = wp_list_pluck( wp_get_sites(), 'blog_id' );
+	if ($blogs) {
+		foreach($blogs as $blog) {
+			switch_to_blog($blog['blog_id']);
+			delete_option( 'floating_related_posts_by_views_or_date_option_name' );
+		}
+		restore_current_blog();
+	}
+} else {	
+	delete_option( 'floating_related_posts_by_views_or_date_option_name' );
+}
