987e7e7a27de2b78ebb6d97985770d43d871e49d
antoniolamorgese <34546111+antoniolamorgese@users.noreply.github.com>
2023-01-29 17:17:48 +0100

    Add files via upload

--- admin/floating-related-posts-by-views-or-date-admin.php
+++ admin/floating-related-posts-by-views-or-date-admin.php
@@ -39,7 +39,7 @@ if(! class_exists('FloatingRelatedPostsByViewsOrDate')){
 
 			<div class="wrap">
 				<h2>Floating Related Posts by Views or Publish Date</h2>
-				<p>Increase website traffic free using Floating Related Posts by Views or Publish Date. Show Floating Related Posts at the bottom or top of your visitor screen. If the <a target='_blank' href='https://wordpress.org/plugins/polylang/' alt='Polylang Plugin Page'>Polylang</a> and <a target='_blank' href='https://wordpress.org/plugins/wp-postviews/' alt='Wp PostViews Plugin Page'>WP PostViews</a> plugins are installed "<b>Floating Related Posts by Views or Publish Date</b>" will randomly fetch all the most visited posts in the current language. Otherwise, the most recent posts will be shown randomly.</p>
+				<p><?php _e("Increase website traffic free using Floating Related Posts by Views or Publish Date. Show Floating Related Posts at the bottom or top of your visitor screen. If the <a target='_blank' href='https://wordpress.org/plugins/polylang/' alt='Polylang Plugin Page'>Polylang</a> and <a target='_blank' href='https://wordpress.org/plugins/wp-postviews/' alt='Wp PostViews Plugin Page'>WP PostViews</a> plugins are installed <b>Floating Related Posts by Views or Publish Date</b> will randomly fetch all the most visited posts in the current language. Otherwise, the most recent posts will be shown randomly.","floating-related-posts-by-views-or-publish-date"); ?></p>
 				<?php settings_errors(); ?>
 				<table>
 					<tr>
@@ -73,14 +73,14 @@ if(! class_exists('FloatingRelatedPostsByViewsOrDate')){
 
 			add_settings_section(
 				'floating_related_posts_by_views_or_date_setting_section', // id
-				'Settings', // title
+				esc_html__('Settings', 'floating-related-posts-by-views-or-publish-date'), // title
 				array( $this, 'floating_related_posts_by_views_or_date_section_info' ), // callback
 				'floating-related-posts-by-views-or-date-admin' // page
 			);
-
+            
 			add_settings_field(
 				'active_status_0', // id
-				'Active Status:', // title
+				esc_html__('Active Status:', 'floating-related-posts-by-views-or-publish-date'), // title
 				array( $this, 'active_status_0_callback' ), // callback
 				'floating-related-posts-by-views-or-date-admin', // page
 				'floating_related_posts_by_views_or_date_setting_section' // section
@@ -88,7 +88,7 @@ if(! class_exists('FloatingRelatedPostsByViewsOrDate')){
 
 			add_settings_field(
 				'desktop_visibility_1', // id
-				'Desktop Visibility:', // title
+				esc_html__('Desktop Visibility:', 'floating-related-posts-by-views-or-publish-date'), // title
 				array( $this, 'desktop_visibility_1_callback' ), // callback
 				'floating-related-posts-by-views-or-date-admin', // page
 				'floating_related_posts_by_views_or_date_setting_section' // section
@@ -96,7 +96,7 @@ if(! class_exists('FloatingRelatedPostsByViewsOrDate')){
 
 			add_settings_field(
 				'mobile_visibility_2', // id
-				'Mobile Visibility:', // title
+				esc_html__('Mobile Visibility:', 'floating-related-posts-by-views-or-publish-date'), // title
 				array( $this, 'mobile_visibility_2_callback' ), // callback
 				'floating-related-posts-by-views-or-date-admin', // page
 				'floating_related_posts_by_views_or_date_setting_section' // section
@@ -104,7 +104,7 @@ if(! class_exists('FloatingRelatedPostsByViewsOrDate')){
 
 			add_settings_field(
 				'excerpt_3', // id
-				'Excerpt:', // title
+				esc_html__('Excerpt:', 'floating-related-posts-by-views-or-publish-date'), // title
 				array( $this, 'excerpt_3_callback' ), // callback
 				'floating-related-posts-by-views-or-date-admin', // page
 				'floating_related_posts_by_views_or_date_setting_section' // section
@@ -112,7 +112,7 @@ if(! class_exists('FloatingRelatedPostsByViewsOrDate')){
 
 			add_settings_field(
 				'background_color_4', // id
-				'Background Color:', // title
+				esc_html__('Background Color:', 'floating-related-posts-by-views-or-publish-date'), // title
 				array( $this, 'background_color_4_callback' ), // callback
 				'floating-related-posts-by-views-or-date-admin', // page
 				'floating_related_posts_by_views_or_date_setting_section' // section
@@ -120,7 +120,7 @@ if(! class_exists('FloatingRelatedPostsByViewsOrDate')){
 
 			add_settings_field(
 				'vertical_position_5', // id
-				'Vertical Position:', // title
+				esc_html__('Vertical Position:', 'floating-related-posts-by-views-or-publish-date'), // title
 				array( $this, 'vertical_position_5_callback' ), // callback
 				'floating-related-posts-by-views-or-date-admin', // page
 				'floating_related_posts_by_views_or_date_setting_section' // section
@@ -128,7 +128,7 @@ if(! class_exists('FloatingRelatedPostsByViewsOrDate')){
 
 			add_settings_field(
 				'horizontal_position_6', // id
-				'Horizontal Position:', // title
+				esc_html__('Horizontal Position:', 'floating-related-posts-by-views-or-publish-date'), // title
 				array( $this, 'horizontal_position_6_callback' ), // callback
 				'floating-related-posts-by-views-or-date-admin', // page
 				'floating_related_posts_by_views_or_date_setting_section' // section
@@ -136,7 +136,7 @@ if(! class_exists('FloatingRelatedPostsByViewsOrDate')){
 
 			add_settings_field(
 				'opacity_7', // id
-				'Opacity:', // title
+				esc_html__('Opacity:', 'floating-related-posts-by-views-or-publish-date'), // title
 				array( $this, 'opacity_7_callback' ), // callback
 				'floating-related-posts-by-views-or-date-admin', // page
 				'floating_related_posts_by_views_or_date_setting_section' // section
@@ -144,7 +144,7 @@ if(! class_exists('FloatingRelatedPostsByViewsOrDate')){
 
 			add_settings_field(
 				'seconds_to_activation_8', // id
-				'Seconds To Activation:', // title
+				esc_html__('Seconds To Activation:', 'floating-related-posts-by-views-or-publish-date'), // title
 				array( $this, 'seconds_to_activation_8_callback' ), // callback
 				'floating-related-posts-by-views-or-date-admin', // page
 				'floating_related_posts_by_views_or_date_setting_section' // section
@@ -152,7 +152,7 @@ if(! class_exists('FloatingRelatedPostsByViewsOrDate')){
 
 			add_settings_field(
 				'seconds_to_deactivation_9', // id
-				'Seconds To Deactivation:', // title
+				esc_html__('Seconds To Deactivation:', 'floating-related-posts-by-views-or-publish-date'), // title
 				array( $this, 'seconds_to_deactivation_9_callback' ), // callback
 				'floating-related-posts-by-views-or-date-admin', // page
 				'floating_related_posts_by_views_or_date_setting_section' // section
@@ -210,28 +210,28 @@ if(! class_exists('FloatingRelatedPostsByViewsOrDate')){
 
 		public function active_status_0_callback() {
 			printf(
-				'<input type="checkbox" name="floating_related_posts_by_views_or_date_option_name[active_status_0]" id="active_status_0" value="active_status_0" %s> <label for="active_status_0">Enable Plugin</label>',
+				'<input type="checkbox" name="floating_related_posts_by_views_or_date_option_name[active_status_0]" id="active_status_0" value="active_status_0" %s> <label for="active_status_0">'.esc_html__('Enable Plugin', 'floating-related-posts-by-views-or-publish-date').'</label>',
 				(($this->$addDefaultValues==1) || ( isset( $this->floating_related_posts_by_views_or_date_options['active_status_0'] ) && $this->floating_related_posts_by_views_or_date_options['active_status_0'] === 'active_status_0' )) ? 'checked' : ''
 			);
 		}
 
 		public function desktop_visibility_1_callback() {
 			printf(
-				'<input type="checkbox" name="floating_related_posts_by_views_or_date_option_name[desktop_visibility_1]" id="desktop_visibility_1" value="desktop_visibility_1" %s> <label for="desktop_visibility_1">Enable Desktop Visibility</label>',
+				'<input type="checkbox" name="floating_related_posts_by_views_or_date_option_name[desktop_visibility_1]" id="desktop_visibility_1" value="desktop_visibility_1" %s> <label for="desktop_visibility_1">'.esc_html__('Enable Desktop Visibility', 'floating-related-posts-by-views-or-publish-date').'</label>',
 				(($this->$addDefaultValues==1) || ( isset( $this->floating_related_posts_by_views_or_date_options['desktop_visibility_1'] ) && $this->floating_related_posts_by_views_or_date_options['desktop_visibility_1'] === 'desktop_visibility_1' )) ? 'checked' : ''
 			);
 		}
 
 		public function mobile_visibility_2_callback() {
 			printf(
-				'<input type="checkbox" name="floating_related_posts_by_views_or_date_option_name[mobile_visibility_2]" id="mobile_visibility_2" value="mobile_visibility_2" %s> <label for="mobile_visibility_2">Enable Mobile Visibility</label>',
+				'<input type="checkbox" name="floating_related_posts_by_views_or_date_option_name[mobile_visibility_2]" id="mobile_visibility_2" value="mobile_visibility_2" %s> <label for="mobile_visibility_2">'.esc_html__('Enable Mobile Visibility', 'floating-related-posts-by-views-or-publish-date').'</label>',
 				(($this->$addDefaultValues==1) || ( isset( $this->floating_related_posts_by_views_or_date_options['mobile_visibility_2'] ) && $this->floating_related_posts_by_views_or_date_options['mobile_visibility_2'] === 'mobile_visibility_2' )) ? 'checked' : ''
 			);
 		}
 
 		public function excerpt_3_callback() {
 			printf(
-				'<input type="checkbox" name="floating_related_posts_by_views_or_date_option_name[excerpt_3]" id="excerpt_3" value="excerpt_3" %s> <label for="excerpt_3">Excerpt include (only Desktop)</label>',
+				'<input type="checkbox" name="floating_related_posts_by_views_or_date_option_name[excerpt_3]" id="excerpt_3" value="excerpt_3" %s> <label for="excerpt_3">'.esc_html__('Excerpt include into banner', 'floating-related-posts-by-views-or-publish-date').'</label>',
 				(($this->$addDefaultValues==1) || ( isset( $this->floating_related_posts_by_views_or_date_options['excerpt_3'] ) && $this->floating_related_posts_by_views_or_date_options['excerpt_3'] === 'excerpt_3' )) ? 'checked' : ''
 			);
 		}
@@ -256,7 +256,7 @@ if(! class_exists('FloatingRelatedPostsByViewsOrDate')){
 				<option value="#fefefe" <?php echo $selected; ?>>Light</option>
 				<?php $selected = (isset( $this->floating_related_posts_by_views_or_date_options['background_color_4'] ) && $this->floating_related_posts_by_views_or_date_options['background_color_4'] === '#d6d8d9') ? 'selected' : '' ; ?>
 				<option value="#d6d8d9" <?php echo $selected; ?>>Dark</option>
-			</select>&nbsp;<a href="https://getbootstrap.com/docs/4.0/components/alerts/" alt="Go to Bootstrap Alerts Color" target="_blank">Go to Bootstrap 4 Alerts Color Code...</a>
+			</select>&nbsp;<a href="https://getbootstrap.com/docs/4.0/components/alerts/" alt="Go to Bootstrap Alerts Color" target="_blank"><?php esc_html_e('Go to Bootstrap 4 Alerts Color Code...', 'floating-related-posts-by-views-or-publish-date'); ?></a>
 			<?php
 		}
 
@@ -265,7 +265,7 @@ if(! class_exists('FloatingRelatedPostsByViewsOrDate')){
 				<?php $selected = (isset( $this->floating_related_posts_by_views_or_date_options['vertical_position_5'] ) && $this->floating_related_posts_by_views_or_date_options['vertical_position_5'] === 'top') ? 'selected' : '' ; ?>
 				<option value="top" <?php echo $selected; ?>>Top</option>
 				<?php $selected = (isset( $this->floating_related_posts_by_views_or_date_options['vertical_position_5'] ) && $this->floating_related_posts_by_views_or_date_options['vertical_position_5'] === 'bottom') ? 'selected' : '' ; ?>
-				<option value="bottom" <?php echo $selected; ?>>Bottom</option>
+				<option value="bottom" <?php echo esc_attr($selected); ?>>Bottom</option>
 			</select> <?php
 		}
 
@@ -288,19 +288,19 @@ if(! class_exists('FloatingRelatedPostsByViewsOrDate')){
 				<option value="Medium" <?php echo $selected; ?>>Medium</option>
 				<?php $selected = (isset( $this->floating_related_posts_by_views_or_date_options['opacity_7'] ) && $this->floating_related_posts_by_views_or_date_options['opacity_7'] === 'High') ? 'selected' : '' ; ?>
 				<option value="High" <?php echo $selected; ?>>High</option>
-			</select>&nbsp;<label for="opacity_7">(Transparency level)</label><?php
+			</select>&nbsp;<label for="opacity_7"><br /><?php esc_html_e('(Transparency level)', 'floating-related-posts-by-views-or-publish-date'); ?></label><?php
 		}
 
 		public function seconds_to_activation_8_callback() {
 			printf(
-				'<input maxlength="4" size="4" class="regular-text" type="text" name="floating_related_posts_by_views_or_date_option_name[seconds_to_activation_8]" id="seconds_to_activation_8" value="%s"><label for="seconds_to_activation_8">Enter a value between 15 and 120.</label>',
+				'<input maxlength="4" size="4" class="regular-text" type="text" name="floating_related_posts_by_views_or_date_option_name[seconds_to_activation_8]" id="seconds_to_activation_8" value="%s"><label for="seconds_to_activation_8"><br />'.esc_html__('Enter a value between 15 and 120.', 'floating-related-posts-by-views-or-publish-date').'</label>',
 				isset( $this->floating_related_posts_by_views_or_date_options['seconds_to_activation_8'] ) ? esc_attr( $this->floating_related_posts_by_views_or_date_options['seconds_to_activation_8']) : '30'
 			);
 		}
 		
 		public function seconds_to_deactivation_9_callback() {
 			printf(
-				'<input maxlength="4" size="4" class="regular-text" type="text" name="floating_related_posts_by_views_or_date_option_name[seconds_to_deactivation_9]" id="seconds_to_deactivation_9" value="%s"><label for="seconds_to_deactivation_9">Enter a value between 15 and 120.</label>',
+				'<input maxlength="4" size="4" class="regular-text" type="text" name="floating_related_posts_by_views_or_date_option_name[seconds_to_deactivation_9]" id="seconds_to_deactivation_9" value="%s"><label for="seconds_to_deactivation_9"><br />'.esc_html__('Enter a value between 15 and 120.', 'floating-related-posts-by-views-or-publish-date').'</label>',
 				isset( $this->floating_related_posts_by_views_or_date_options['seconds_to_deactivation_9'] ) ? esc_attr( $this->floating_related_posts_by_views_or_date_options['seconds_to_deactivation_9']) : '15'
 			);
 		}	
